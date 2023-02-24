c99a2330fda969a30f17066e808b1e9f2f3ca3ac
taylorwood95 <115211674+taylorwood95@users.noreply.github.com>
2023-02-24 11:40:27 +0000

    app.js and container updated

---
+++ src/containers/GuardianContainer.js
@@ -0,0 +1,28 @@
+import React, {useEffect, useState} from "react";
+
+
+
+const GuardianContainer = () => {
+
+    const [articles, setArticles] = useState([])
+
+    useEffect(() => {
+        getArticles()
+    }, [])
+
+    const getArticles = () => {
+        fetch("https://content.guardianapis.com/search?q=brexit&format=json&api-key=test")
+        .then(results => results.json)
+        .then(articlesData => setArticles(articlesData))
+    }
+
+    return (
+        <div>
+            <h1>Guardian Container</h1>
+            <ArticleList />
+        </div>
+    )
+}
+
+
+export  default GuardianContainer
